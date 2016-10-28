edc7f6a9ee3258adb983dd860890906387f7e385
Montak Oleg <montak.o@ya.ru>
2016-10-29 01:49:16 +0300

    Training by Irvin

--- RapHelper/scripts/generate_db.sh
+++ RapHelper/scripts/generate_db.sh
@@ -14,5 +14,7 @@ echo "Training by Miron..." && ./parse_text.py "seed/miron.txt"
 echo "Training by Leningrad..." && ./parse_text.py "seed/leningrad.txt"
 echo "Training by Versus..." && ./parse_text.py "seed/versus.txt"
 echo "Training by Pelevin" && ./parse_text.py "seed/pelevin.txt"
+echo "Training by Irvin" && ./parse_text.py "seed/irvin_acid_house.txt"
+echo "Training by Irvin" && ./parse_text.py "seed/irvin_extasy.txt"
 
 echo "Done. See results in words.db. See missing words in missing_words.txt"
