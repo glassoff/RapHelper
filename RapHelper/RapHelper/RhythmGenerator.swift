63b5c00c7332eba3881bc2760865d4f4d683206b
Evgeny Petrov <tromg1@gmail.com>
2016-10-28 15:18:31 +0300

    RhythmGenerator added

---
+++ RapHelper/RapHelper/RhythmGenerator.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+class RhythmGenerator: NSObject {
+
+    static func rhythm(with string: String) -> [Bool] {
+        let vowelsStr = "а, о, э, и, у, ы, е, ё, ю, я"
+        let vowels = vowelsStr.components(separatedBy: ", ")
+
+        let accent = "'".characters.first!
+
+        let words = string.components(separatedBy: CharacterSet.whitespacesAndNewlines)
+
+        var syllables: [Bool?] = []
+
+        for word in words {
+            let onlyVowels = word.characters.filter{vowels.contains(String($0)) || $0 == "'"}
+
+            if onlyVowels.count == 1 {
+                syllables.append(nil)
+                continue
+            }
+            // TODO: добавить логику для односложных слов
+            for vowel in onlyVowels {
+                if vowel == "ё" {
+                    syllables.append(true)
+                } else if vowel == accent {
+                    syllables.removeLast()
+                    syllables.append(true)
+                } else {
+                    syllables.append(false)
+                }
+            }
+        }
+
+        let rhythmes = ["0001", "0010", "0100", "1000", "100", "010", "001", "01", "10"]
+
+        var goodRhythmes: [String] = []
+
+        for rhymeToTest in rhythmes {
+            let rhymeToTestCharacters = rhymeToTest.characters.map{String($0)}
+
+            if syllables.count/rhymeToTestCharacters.count < 2 {
+                continue
+            }
+
+            var rhymeToTestIsGood = true
+
+            for (index, syllable) in syllables.enumerated() {
+                if let syllable = syllable {
+                    let testCharacter = rhymeToTestCharacters[index % rhymeToTestCharacters.count]
+                    if testCharacter == "0" && syllable || testCharacter == "1" && !syllable {
+                        rhymeToTestIsGood = false
+                        break
+                    }
+                }
+            }
+
+            if rhymeToTestIsGood {
+                goodRhythmes.append(rhymeToTest)
+            }
+        }
+        
+        let rhymeToReturnCharacters = (goodRhythmes.first ?? rhythmes.last!).characters.map{String($0)}
+        
+        var boolRhymeToReturn: [Bool] = []
+        
+        for i in 0..<syllables.count {
+            let rhymeChar = rhymeToReturnCharacters[i % rhymeToReturnCharacters.count]
+            if rhymeChar == "0" {
+                boolRhymeToReturn.append(false)
+            } else {
+                boolRhymeToReturn.append(true)
+            }
+        }
+
+        return boolRhymeToReturn
+    }
+}
