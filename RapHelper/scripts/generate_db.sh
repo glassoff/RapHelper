f175c06243566867db2325a2fa90231a6cd90fbe
Montak Oleg <montak.o@ya.ru>
2016-10-29 02:18:51 +0300

    Training by Tolstoy

--- RapHelper/scripts/generate_db.sh
+++ RapHelper/scripts/generate_db.sh
@@ -10,11 +10,15 @@ echo "Parsing fucks dictionary..."
 echo "Parsing corrections dictionary..."
 ./parse_dict.py "seed/corrected_dict.txt" 1
 
+echo "Parsing Tolstoy corrections dictionary..."
+./parse_dict.py "seed/voina_corrected_dict.txt" 0
+
 echo "Training by Miron..." && ./parse_text.py "seed/miron.txt"
 echo "Training by Leningrad..." && ./parse_text.py "seed/leningrad.txt"
 echo "Training by Versus..." && ./parse_text.py "seed/versus.txt"
-echo "Training by Pelevin" && ./parse_text.py "seed/pelevin.txt"
-echo "Training by Irvin" && ./parse_text.py "seed/irvin_acid_house.txt"
-echo "Training by Irvin" && ./parse_text.py "seed/irvin_extasy.txt"
+echo "Training by Pelevin..." && ./parse_text.py "seed/pelevin.txt"
+echo "Training by Irvin..." && ./parse_text.py "seed/irvin_acid_house.txt"
+echo "Training by Irvin..." && ./parse_text.py "seed/irvin_extasy.txt"
+echo "Training by Tolstoy..." && ./parse_text.py "seed/voina1.txt"
 
-echo "Done. See results in words.db. See missing words in missing_words.txt"
+echo "\nDone. See results in words.db. See missing words in missing_words.txt"
