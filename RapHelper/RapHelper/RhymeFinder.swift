78dfc42d036e104bbb643f05624d1d9f488d1667
Evgeny Petrov <tromg1@gmail.com>
2016-10-29 02:34:22 +0300

    fix

--- RapHelper/RapHelper/RhymeFinder.swift
+++ RapHelper/RapHelper/RhymeFinder.swift
@@ -5,24 +5,27 @@ class RhymeFinder {
     static func find(_ word: String) -> [Word] {
         let dbQueue = (UIApplication.shared.delegate as! AppDelegate).dbQueue
 
+        var words = [Word]()
+
         var result: FMResultSet?
         dbQueue?.inDatabase({ (db) in
             result = try! db!.executeQuery("SELECT * FROM words where suffix = \"\(word)\"", values: nil)
+
+            if let result = result {
+                while result.next() == true {
+                    let word = Word()
+                    word.accentText = result.string(forColumn: "word_accent")
+                    word.text = result.string(forColumn: "word")
+                    word.priority = result.int(forColumn: "priority")
+                    words.append(word)
+                }
+
+                result.close()
+            }
+
         })
         
-        var words = [Word]()
 
-        if let result = result {
-            while result.next() == true {
-                let word = Word()
-                word.accentText = result.string(forColumn: "word_accent")
-                word.text = result.string(forColumn: "word")
-                word.priority = result.int(forColumn: "priority")
-                words.append(word)
-            }
-            
-            result.close()
-        }
 
         return words
     }
