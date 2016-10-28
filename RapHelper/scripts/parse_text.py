59c0437951a45862e2b9607b31beb70438844099
Montak Oleg <montak.o@ya.ru>
2016-10-28 22:54:08 +0300

    Correct parse newlines for linebreaks texts, allow '-' in words

--- RapHelper/scripts/parse_text.py
+++ RapHelper/scripts/parse_text.py
@@ -6,7 +6,7 @@ import re
 import appdb
 import codecs
 
-clear_pattern = re.compile('[\W]+')
+clear_pattern = re.compile('[^\w-]+')
 sentence_ends = set(['!', '?', '.', '…'])
 SENTENCE_CONST = "*new_sentence*"
 
@@ -36,34 +36,31 @@ def process_word(word, prev_word, result):
     word_info = result[word]
     word_info.count += 1
 
-    if not (prev_word is None):
-        if sentence_start(prev_word):
-            inc_by_key(SENTENCE_CONST, word_info.related)
-        else:
-            clear_prev_word = clear_word(prev_word)
-            inc_by_key(clear_prev_word, word_info.related)
-    else:
+    if (prev_word is None) or sentence_start(prev_word):
         inc_by_key(SENTENCE_CONST, word_info.related)
+    else:
+        clear_prev_word = clear_word(prev_word)
+        inc_by_key(clear_prev_word, word_info.related)
 
 
-def process_line(line, result):
-    words = line.split()
+def process_text(text, result):
+    words = text.split()
     if len(words) == 0:
         return
 
     for index in range(1, len(words) - 1):
         process_word(words[index], words[index - 1], result)
 
-    # proceed first word in line
-    if len(words[0]) > 0:
+    # proceed first word
+    if len(words) > 0:
         process_word(words[0], None, result)
 
 
 def parse_file(fname):
     result = {}
     with codecs.open(fname, 'r', 'utf8') as f:
-        for line in f:
-            process_line(line, result)
+        text = f.read()
+    process_text(text, result)
     return result
 
 def get_word_id(db, word):
@@ -77,7 +74,7 @@ def save_result_to_db(result):
     db = appdb.connect()
 
     db.execute('BEGIN TRANSACTION')
-    guaranteeSentence(db)
+    guarantee_sentence_word_exists(db)
 
     for word in result:
         for related_word in result[word].related:
@@ -103,7 +100,7 @@ def save_result_to_db(result):
     db.close()
 
 
-def guaranteeSentence(db):
+def guarantee_sentence_word_exists(db):
     db.execute('INSERT OR IGNORE INTO words (word, word_accent, suffix, count, priority) VALUES (?, ?, ?, ?, ?)', (SENTENCE_CONST, SENTENCE_CONST, SENTENCE_CONST, 0, 0))
 
 def main():
